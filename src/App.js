13ba5bb458f762ee0455583c1594f96964b95bbc
sbodrero <bodrero.sebastien@gmail.com>
2021-08-19 11:59:25 +0200

    Added auth

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,8 +19,9 @@ function App() {
           Learn React now
         </a>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
